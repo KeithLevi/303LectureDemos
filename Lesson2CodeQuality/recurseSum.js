a98c1689e94e3c5f16e1d7bc629f6ee9be0bcd25
KeithLevi <guilddrive@gmail.com>
2019-10-01 23:54:25 -0500

    from laptop

---
+++ Lesson2CodeQuality/recurseSum.js
@@ -0,0 +1,12 @@
+function recurseSum(array) {
+
+    if (array.length === 1) {
+        return array[0];
+    } else {
+        return array.pop() + recurseSum(array);
+    }
+
+    
+}
+
+console.log(recurseSum([1,2,3,4]));
