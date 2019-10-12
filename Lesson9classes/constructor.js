d5160f45a52056866e9fb929fe6c40724acc605e
Keith Levi <guilddrive@gmail.com>
2019-10-11 22:23:37 -0500

    class User example

---
+++ Lesson9classes/constructor.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+/**
+ * 
+ */
+class User {
+    constructor(name = "Anonymous") {
+        this.name = name;
+    }
+    sayHi() {
+        console.log(`Hello, ${this.name}!`);
+    }
+}
+fred = new User();
+console.log(fred);
+console.log(fred.__proto__);
+console.log(User.prototype);
+fred.sayHi();
+bob = new User("Bob");
+console.log(bob);
+bob.sayHi();
